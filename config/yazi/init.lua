ed761be8bc28d7ffda2e36f4b7e96ac4b20dea9e
Paul Cameron <116021619+paul-s-cameron@users.noreply.github.com>
2025-03-20 02:26:38 -0400

    yazi backup

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -4,3 +4,26 @@ require("copy-file-contents"):setup({
 	append_char = "\n",
 	notification = true,
 })
+
+require("augment-command"):setup({
+    prompt = false,
+    default_item_group_for_prompt = "hovered",
+    smart_enter = true,
+    smart_paste = false,
+    smart_tab_create = false,
+    smart_tab_switch = false,
+    confirm_on_quit = true,
+    open_file_after_creation = false,
+    enter_directory_after_creation = false,
+    use_default_create_behaviour = false,
+    enter_archives = false,
+    extract_retries = 3,
+    recursively_extract_archives = false,
+    preserve_file_permissions = false,
+    must_have_hovered_item = true,
+    skip_single_subdirectory_on_enter = true,
+    skip_single_subdirectory_on_leave = true,
+    smooth_scrolling = false,
+    scroll_delay = 0.02,
+    wraparound_file_navigation = false,
+})
