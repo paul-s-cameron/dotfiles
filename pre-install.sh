ead6227a815b45194e80d4d2beabdf1a999b9e1c
Paul Cameron <116021619+paul-s-cameron@users.noreply.github.com>
2025-03-21 17:11:21 -0400

    pre install refactor
    fixed chsh command

--- pre-install.sh
+++ pre-install.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
+if ! command -v yay &> /dev/null; then
+  echo "Installing yay"
+
+  # Dependencies
+  sudo pacman -S --needed git base-devel
+
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+
+  # Cleanup
+  cd ..
+  rm -r yay
+
+  echo "Yay installation complete"
+else
+  echo "Yay is installed"
+fi
+
+echo "Installing packages"
+yay -S --needed --noconfirm - < ~/.dotfiles/packages |& grep -v "skipping"
+
 # Check if oh-my-zsh is installed
-OMZDIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZDIR" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
   echo 'Updating oh-my-zsh'
-  omz update
+  $ZSH/tools/upgrade.sh
 fi
 
 # Change default shell
-if [! $0 = "-zsh"]; then
+if [[ ${SHELL##*/} != "zsh" ]]; then
   echo 'Changing default shell to zsh'
-  chsh -s /bin/zsh
+  sudo chsh -s /bin/zsh $(whoami)
 else
   echo 'Already using zsh'
 fi
