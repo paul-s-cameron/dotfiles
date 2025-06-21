7db5eda71be5f6bbb64da851f98431fdb0251a3e
Paul Cameron <116021619+paul-s-cameron@users.noreply.github.com>
2025-06-20 22:13:01 -0400

    fastfetch

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define a file to store the current playlist path
-playlist_env_file="$HOME/.wallpaper_playlist"
+playlist_env_file="$HOME/.wallpaper"
 
 send_command() {
   local my_command=$1
