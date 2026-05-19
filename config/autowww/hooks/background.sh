315adc4e8b18014ef5c5c8b2a108fb439491ba54
Paul Cameron <116021619+paul-s-cameron@users.noreply.github.com>
2026-05-19 01:27:24 -0400

    custom autowww wallpaper watcher

---
+++ config/autowww/hooks/background.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -sf "$(autowww current)" $HOME/.config/background
---
+++ config/autowww/hooks/vesktop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cat $HOME/.cache/wal/colors-discord.css > $HOME/.config/vesktop/themes/colors-discord.css
+
